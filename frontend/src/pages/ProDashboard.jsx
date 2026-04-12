235040a4a458a59f3f1038a37e1866f8ca86ede5
nael05 <nael.morellon@ynov.com>
2026-04-12 22:13:54 +0200

    fix: Force rear camera for scanner

--- frontend/src/pages/ProDashboard.jsx
+++ frontend/src/pages/ProDashboard.jsx
@@ -108,8 +108,11 @@ function ProDashboard() {
           fps: 15, 
           qrbox: { width: 250, height: 250 },
           aspectRatio: 1.0,
-          rememberLastUsedCamera: true,
-          showTorchButtonIfSupported: true
+          rememberLastUsedCamera: false, // On force le nouveau réglage
+          showTorchButtonIfSupported: true,
+          videoConstraints: {
+            facingMode: 'environment'
+          }
         },
         false
       )
